70f9c5187eb3368b62b2239f63c09fb8951f7b7b
emjo0002 <143484536+emjo0002@users.noreply.github.com>
2026-06-11 05:38:18 +0200

    database

--- x.sql
+++ x.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mariadb
--- Generation Time: Dec 09, 2025 at 12:29 PM
+-- Generation Time: Jun 11, 2026 at 03:35 AM
 -- Server version: 10.6.20-MariaDB-ubu2004
 -- PHP Version: 8.2.27
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `x`
 --
+CREATE DATABASE IF NOT EXISTS `x` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `x`;
 
 -- --------------------------------------------------------
 
@@ -38,6 +40,7 @@ CREATE TABLE `bookmarks` (
 --
 
 INSERT INTO `bookmarks` (`bookmark_user_fk`, `bookmark_post_fk`, `bookmarked_at`) VALUES
+('4542f5410310486b9efecc4934e1a643', '6fb859f7093543148f922a08ddd649a3', 1781131874),
 ('a5d53c2b385341e88b644a1bbbd8e131', '5c4a1ea51cc2472cbe27f030c2073b2e', 1765280007),
 ('b09b7853689b490a9c1e093b0507dc8b', '3480c1614d6f44d59534f5da6f38cf56', 1765281819);
 
@@ -61,6 +64,7 @@ CREATE TABLE `comments` (
 --
 
 INSERT INTO `comments` (`comment_pk`, `comment_post_fk`, `comment_user_fk`, `comment_text`, `comment_updated_at`, `comment_created_at`) VALUES
+('91db41a460c6431a929d97ba52b25922', '6fb859f7093543148f922a08ddd649a3', '4542f5410310486b9efecc4934e1a643', 'I can still see it hehe', 0, 1781131872),
 ('b48c8e9498a24463919d3fdfc727d13d', 'd7d810e76ed845a9aae051e0189495e7', 'b09b7853689b490a9c1e093b0507dc8b', 'Messi is better btw;)', 0, 1765281846),
 ('ba22fc97ef46476a99513d0e8fe29e55', 'c4fa0640c5784ff1af6ac0ac06b03997', 'a5d53c2b385341e88b644a1bbbd8e131', 'Welcome aboard, buddy!', 0, 1765279985);
 
@@ -102,7 +106,8 @@ CREATE TABLE `follows` (
 
 INSERT INTO `follows` (`follow_follower_fk`, `follow_followed_fk`, `follow_timestamp`) VALUES
 ('9860c6174a3141c5b1e7c8b3638b2f2b', '88a93bb5267e443eb0047f421a7a2f34', 1765100873),
-('b09b7853689b490a9c1e093b0507dc8b', '9860c6174a3141c5b1e7c8b3638b2f2b', 1765281336);
+('b09b7853689b490a9c1e093b0507dc8b', '9860c6174a3141c5b1e7c8b3638b2f2b', 1765281336),
+('fe9715d110534b17a9dc36e1f8dfd43c', 'b09b7853689b490a9c1e093b0507dc8b', 1780952007);
 
 -- --------------------------------------------------------
 
@@ -162,21 +167,24 @@ CREATE TABLE `posts` (
   `post_blocked_at` bigint(20) DEFAULT NULL,
   `post_deleted_at` bigint(20) NOT NULL,
   `post_updated_at` bigint(20) NOT NULL,
-  `post_created_at` int(11) NOT NULL
+  `post_created_at` int(11) NOT NULL,
+  `post_visibility` enum('public','private') NOT NULL DEFAULT 'public'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `posts`
 --
 
-INSERT INTO `posts` (`post_pk`, `post_user_fk`, `post_message`, `post_total_likes`, `post_total_comments`, `post_media_path`, `post_blocked_at`, `post_deleted_at`, `post_updated_at`, `post_created_at`) VALUES
-('3480c1614d6f44d59534f5da6f38cf56', '9860c6174a3141c5b1e7c8b3638b2f2b', 'I don\'t know why, but I like sofas so much, I have made my profile picture a sofa-cushion :)', 2, 0, '', 0, 0, 0, 1765278686),
-('5c4a1ea51cc2472cbe27f030c2073b2e', 'b09b7853689b490a9c1e093b0507dc8b', 'I\'m so happy that Malthe finally spoke out about his love for sofas! Honesty is good!', 1, 0, '', 0, 0, 0, 1765278933),
-('85a3d8a9864b47298290aa90576b9285', '88a93bb5267e443eb0047f421a7a2f34', 'Hi. I\'m Gustav. I am a medieval time-traveller!', 0, 0, '', 0, 0, 0, 1765278640),
-('c4fa0640c5784ff1af6ac0ac06b03997', '12d6e25ee4a1401d9e5575d6319ce496', 'I\'m new to X, so be kind!', 1, 1, '', 0, 0, 0, 1765278470),
-('d7d810e76ed845a9aae051e0189495e7', 'a5d53c2b385341e88b644a1bbbd8e131', 'I\'m such a big Cristiano Ronaldo SUIIII fan!', 0, 1, 'images/294f19dc9b784be2a422368ca964f574_cristi.jpg', 0, 0, 0, 1765279953),
-('ebfce8bbac524094b3a9fa169f6eb029', '87df12f908664075aa97a68cb4f5280f', 'I\'m the president!!', 0, 0, '', 0, 0, 0, 1765280856),
-('fddbc0c31c63431e8fd5ff8fc63e7aea', 'a5d53c2b385341e88b644a1bbbd8e131', 'I swear to god, I hate Chelsea & Arsenal fans! They\'re so fucking stubborn GRRRRRR ARGHHH', 0, 0, '', 1765280414, 0, 0, 1765280365);
+INSERT INTO `posts` (`post_pk`, `post_user_fk`, `post_message`, `post_total_likes`, `post_total_comments`, `post_media_path`, `post_blocked_at`, `post_deleted_at`, `post_updated_at`, `post_created_at`, `post_visibility`) VALUES
+('3480c1614d6f44d59534f5da6f38cf56', '9860c6174a3141c5b1e7c8b3638b2f2b', 'I don\'t know why, but I like sofas so much, I have made my profile picture a sofa-cushion :)', 2, 0, '', 0, 0, 0, 1765278686, 'public'),
+('5c4a1ea51cc2472cbe27f030c2073b2e', 'b09b7853689b490a9c1e093b0507dc8b', 'I\'m so happy that Malthe finally spoke out about his love for sofas! Honesty is good!', 1, 0, '', 0, 0, 0, 1765278933, 'public'),
+('6fb859f7093543148f922a08ddd649a3', 'b09b7853689b490a9c1e093b0507dc8b', 'This is private hehe', 0, 1, '', 0, 0, 1781131824, 1781129016, 'private'),
+('707c22f2362f4d7b8717535096223199', 'b09b7853689b490a9c1e093b0507dc8b', '<script>alert(1)</script>', 0, 0, '', 0, 0, 0, 1781132011, 'public'),
+('85a3d8a9864b47298290aa90576b9285', '88a93bb5267e443eb0047f421a7a2f34', 'Hi. I\'m Gustav. I am a medieval time-traveller!', 0, 0, '', 0, 0, 0, 1765278640, 'public'),
+('c4fa0640c5784ff1af6ac0ac06b03997', '12d6e25ee4a1401d9e5575d6319ce496', 'I\'m new to X, so be kind!', 1, 1, '', 0, 0, 0, 1765278470, 'public'),
+('d7d810e76ed845a9aae051e0189495e7', 'a5d53c2b385341e88b644a1bbbd8e131', 'I\'m such a big Cristiano Ronaldo SUIIII fan!', 0, 1, 'images/294f19dc9b784be2a422368ca964f574_cristi.jpg', 0, 0, 0, 1765279953, 'public'),
+('ebfce8bbac524094b3a9fa169f6eb029', '87df12f908664075aa97a68cb4f5280f', 'I\'m the president!!', 0, 0, '', 0, 0, 0, 1765280856, 'public'),
+('fddbc0c31c63431e8fd5ff8fc63e7aea', 'a5d53c2b385341e88b644a1bbbd8e131', 'I swear to god, I hate Chelsea & Arsenal fans! They\'re so fucking stubborn GRRRRRR ARGHHH', 0, 0, '', 1765280414, 0, 0, 1765280365, 'public');
 
 -- --------------------------------------------------------
 
@@ -238,7 +246,8 @@ INSERT INTO `users` (`user_pk`, `user_email`, `user_password`, `user_password_re
 ('88a93bb5267e443eb0047f421a7a2f34', 'santi@gmail.com', 'scrypt:32768:8:1$PEIO0eliDPqnCCbw$acb791128831bc90030ac363e4b76db196689bd99c1ccde5c2c20a7d4fe909e07129f3f4fd4f086e347375edbb8229e9ba5dc126cc14f6107fb1fc2abf6498f8', NULL, 'GustavDev', 'Gustav', 'Larsen', 'static/images/avatars/b03959e8afd84bdd9270fcaca03810d7.jpg', '', 54654564, 0, 0, 0),
 ('9860c6174a3141c5b1e7c8b3638b2f2b', 'maltheaaen@gmail.com', 'scrypt:32768:8:1$5NSH8Gsqi83lQV24$b61989755f5e00e7632463dee7b806b93acab7d4de36b6e32caf47a2fcef8bf23db0624a3767d5bae3ba40c77673171dad51a4b472e44a9463fc141a0b7f37bb', NULL, 'Malt', 'Malthe', 'Aaen', 'static/images/avatars/a646c5c464854943b27d7c07c0074fb8.png', '', 54654564, 0, 0, 0),
 ('a5d53c2b385341e88b644a1bbbd8e131', 'r@r.dk', 'scrypt:32768:8:1$0RU5B87Om1vuTrj5$19c6e65314a2bb8b96444834b1901e1cd38594bfaaf85ae378034f8978e0338ed0b7d3076d26545b0ed176fa1a10c503b4ff2f7807ac5164703cddc315d583fd', '', 'RasmusOlsen', 'Rasmus', 'Olsen', 'static/images/avatars/882972796125456fb54ec7ff1109804e.jpg', '', 1765277199, 0, 0, 0),
-('b09b7853689b490a9c1e093b0507dc8b', 'emil.johansson2@gmail.com', 'scrypt:32768:8:1$OGl7y8BaCfoVx7xc$7ed1d28e8ab742516d793102dc43d5056f59dcdb5a46689925702e3a41bee1a7a534a54dbbc08d98f8f10d80e306bc09f5826451e308284e1151b1636f3e305b', '', 'EmilDev', 'Emil', 'Johansson', 'static/images/avatars/b54e32cb4a054b1cb3b2da9821580653.jpg', '', 54654564, 0, 0, 0);
+('b09b7853689b490a9c1e093b0507dc8b', 'emil.johansson2@gmail.com', 'scrypt:32768:8:1$OGl7y8BaCfoVx7xc$7ed1d28e8ab742516d793102dc43d5056f59dcdb5a46689925702e3a41bee1a7a534a54dbbc08d98f8f10d80e306bc09f5826451e308284e1151b1636f3e305b', '', 'DevEmil', 'Emil', 'Johansson', 'static/images/avatars/b54e32cb4a054b1cb3b2da9821580653.jpg', '', 54654564, 0, 0, 0),
+('fe9715d110534b17a9dc36e1f8dfd43c', 'emjo0002@stud.kea.dk', 'scrypt:32768:8:1$jIUdJNvmKkfPEPyK$ee34500bde95557468ca80a00aa0ce226fe0e6191b6a3710781b15799a119f6c9e124490a552af7ad3989fe8b95b18fd161be65fd974b517c3aa08fc17cf95ef', '', 'emil8883', 'Emil', 'Johansson', 'static/images/avatars/unknown.jpg', '', 1765277199, 1780952655, 0, 0);
 
 --
 -- Indexes for dumped tables
